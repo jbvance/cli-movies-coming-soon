93d009774be3f61ae7aafd9414df701ccbd833ce
Jason Vance <jbvance@gmail.com>
2017-12-02 04:15:29 +0000

    add comments

--- lib/movie_list/movie.rb
+++ lib/movie_list/movie.rb
@@ -4,15 +4,18 @@ class MovieList::Movie
     
     def initialize
         puts "Movie class Initiated"
+        # Store the html from the url
          doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
+         
+        # Create the class level variable for all the movies
         movies = doc.css('#main').css('.list').css('div.list_item')
         movies.each_with_index do |movie, index|
             @@all << movie
-            
         end
     end
     
     def self.list_movies
+        # Loop through all movies and display number and title
         @@all.each_with_index do |movie, index|
             puts "#{index + 1}. #{@@all[index].css('h4').children.attr('title').value}"
         end
