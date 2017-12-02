f43166f8c6955d2f0578f43e96cad5e33e4e7a68
Jason Vance <jbvance@gmail.com>
2017-12-02 03:31:03 +0000

    Refactor list_movies and add header and footer

--- lib/movie_list/cli.rb
+++ lib/movie_list/cli.rb
@@ -8,23 +8,38 @@ class MovieList::CLI
     end
     
     def call
-        puts "Movie List"
         list_movies
+       puts "Enter the number of the movie you would like to view, or type 'exit'"
+    end
+    
+    def header
+       puts "---------------------MOVIES COMING SOON------------------------" 
+    end
+    
+    def footer
+        puts "--------------------------------------------------------------"
     end
     
     def list_movies
-        menu
+        header
+        movie_list = []
+        doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
+        movies = doc.css('#main').css('.list').css('div.list_item')
+        #code to get first movie title
+        movies.each_with_index do |movie, index|
+            puts "#{index + 1}. #{movies[index].css('h4').children.attr('title').value}"
+        end
+        footer
+        
+        #binding.pry
+        MovieList::Movie.new
     end
     
     def menu
         #display movies coming soon
         puts "Enter your choice number or type 'exit'"
         input = gets.strip.downcase
-        doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
-        movies = doc.css('#main').css('.list').css('div.list_item')
-        #code to get first movie title
-        #movies[0].css('h4').children.attr('title').value
-        #binding.pry
+        
         
         while input != 'exit'
             puts "You entered #{input}"
--- lib/movie_list/movie.rb
+++ lib/movie_list/movie.rb
@@ -1,3 +1,5 @@
 class MovieList::Movie
-    
+    def initialize
+        puts "Movie Initiated"
+    end
 end
